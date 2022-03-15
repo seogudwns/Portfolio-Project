7533a8bd721032001c586972e69f52bec9ff489a
haechan <kiki9510@gmail.com>
2022-03-15 18:11:18 +0900

    award 스키마 작성 및 Award 모델 뼈대 코드 작성 완료

---
+++ back/src/db/models/Award.js
@@ -0,0 +1,21 @@
+import { AwardModel } from "../schemas/award";
+
+class Award {
+    static async create() {
+        return
+    }
+
+    static async findById() {
+        return
+    }
+
+    static async udpate() {
+        return
+    }
+
+    static async findByUserId() {
+        return
+    }
+}
+
+export { Award };
