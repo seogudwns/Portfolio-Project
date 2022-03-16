a5588a4af3dc2e0cf777ded3eac278c996e33072
haechan <kiki9510@gmail.com>
2022-03-16 17:53:48 +0900

    Education model 생성, id 조회 작성

--- back/src/db/models/Education.js
+++ back/src/db/models/Education.js
@@ -1,12 +1,14 @@
 import { EducationModel } from "../schemas/education";
 
 class Education {
-    static async create() {
-
+    static async create({ educationData }) {
+        const createdNewEdu = await EducationModel.create(educationData);
+        return createdNewEdu;
     }
 
-    static async findById() {
-
+    static async findById({ education_id }) {
+        const education = await EducationModel.findOne({ id: education_id });
+        return education;
     }
 
     static async update() {
