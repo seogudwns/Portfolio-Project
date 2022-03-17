9b4a405e8654f01ada1bd1430041df3caaf0717e
leewol <lsee5265@gmail.com>
2022-03-17 20:48:08 +0900

    update Portfolio.js

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -5,6 +5,8 @@ import { Container, Col, Row } from "react-bootstrap";
 import { UserStateContext } from "../App";
 import * as Api from "../api";
 import User from "./user/User";
+import Educations from "./education/Educations";
+import Awards from "./awards/Awards";
 
 function Portfolio() {
   const navigate = useNavigate();
@@ -61,11 +63,20 @@ function Portfolio() {
           />
         </Col>
         <Col>
-
-          <div style={{ textAlign: "center" }}>
-            학력 목록, 수상이력 목록, 프로젝트 목록, 자격증 목록 만들기
-          </div>
-
+          <Educations 
+            portfolioOwnerId={portfolioOwner.id}
+            isEditable={portfolioOwner.id === userState.user?.id}
+          />
+        </Col>
+          <Awards 
+              portfolioOwnerId={portfolioOwner.id}
+              isEditable={portfolioOwner.id === userState.user?.id}
+            />
+        <Col>
+        </Col>
+        <Col>
+        </Col>
+        <Col>
         </Col>
       </Row>
     </Container>
