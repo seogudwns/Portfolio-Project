2dfd12a3cc711a741c6ebe8b4ba0e7850ad2487c
HyoungJun Seo <seogudwns12@gmail.com>
2022-03-18 22:31:11 +0900

    미완

---
+++ back/src/routers/projectRouters.js
@@ -0,0 +1,122 @@
+import is from "@sindresorhus/is";
+import { Router } from "express";
+import { login_required } from "../middlewares/login_required";
+import { projectAuthService } from "../services/projectServices";
+
+const projectAuthRouter = Router();
+
+//******************************************************** 1. project 생성
+projectAuthRouter.post(
+  "/project/create",
+  login_required,
+  async function (req, res, next) {
+    try {
+      if (is.emptyObject(req.body)) {
+        throw new Error(
+          "headers의 Content-Type을 application/json으로 설정해주세요"
+        );
+      }
+
+      // req 에서 데이터 가져오기
+      const user_id = req.currentUserId;
+      const title = req.body.title;
+      const description = req.body.description ?? null;
+      const from_date = req.body.from_date;
+      const to_date = req.body.to_date;
+
+
+      //db에 추가
+      const newProject = await projectAuthService.addProject({
+        user_id,
+        title,
+        description,
+        from_date,
+        to_date
+      });
+
+      if (newProject.errorMessage) {
+        throw new Error(newProject.errorMessage);
+      }
+
+      res.status(201).json(newProject);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//******************************************************** 4. user_id : user_id를 포함한 모든 project 가져오기. ~
+projectAuthRouter.get(
+  "/projectlists/:user_id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      // jwt토큰에서 추출된 사용자 id를 가지고 db에서 사용자 정보를 찾음.
+      const user_id = req.params.user_id;
+      const currentProjectInfo = await projectAuthService.getProjectInfo({
+        user_id
+      });
+
+      if (currentProjectInfo.errorMessage) {
+        throw new Error(currentProjectInfo.errorMessage);
+      }
+
+      res.status(200).send(currentProjectInfo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+
+//********************************************************************* 3. 수정기능
+projectAuthRouter.put(
+  "/projects/:id",
+  login_required,
+  async function (req, res, next) {
+    try {
+      // req에서 데이터 가져오기
+      const project_id = req.params.id;
+      const title = req.body.title ?? null;
+      const description = req.body.description ?? null;
+      const from_date = req.body.from_date;
+      const to_date = req.body.to_date;
+
+      const toUpdate = { title, description, from_date, to_date };
+
+      const updateProject = await projectAuthService.setProject({ project_id, toUpdate });
+      //묶은 후 user_id를 통해 업데이트 진행, 업데이트 요소가 없을 시 기존 자료가 저장됨.
+
+      if (updateProject.errorMessage) {
+        throw new Error(updateProject.errorMessage);
+      }
+
+      res.status(200).send(updateProject);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+
+//! 자격증과 마찬가지로 현재 이게 있는 의미를 모르겠기에 login_required는 제외
+projectAuthRouter.get('/projects/:id',
+  async function (req, res, next) {
+    try {
+      const project_id = req.params.id;
+
+      const searchProject = await projectAuthService.getProject(project_id);
+
+      if (searchProject.errorMessage) {
+        throw new Error(searchProject.errorMessage);
+      }
+
+      res.status(200).send(searchProject);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+
+export { projectAuthRouter };
