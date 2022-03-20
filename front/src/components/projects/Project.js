57ba5200af2ca58064dd3cdbc3b1b033b82a4fd1
Hayeong Kim <fibonacci2246@gmail.com>
2022-03-20 17:59:20 +0900

    fix: ready to api connect

--- front/src/components/projects/Project.js
+++ front/src/components/projects/Project.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
-import ProjectEditForm from './ProjectEditForm';
-import ProjectCard from './ProjectCard';
+import React, { useState } from "react";
+import ProjectEditForm from "./ProjectEditForm";
+import ProjectCard from "./ProjectCard";
 
 function Project({ project, isEditable }) {
-    const [ isEditing, setIsEditing ] = useState(false);
-    const [ thisProject, setThisProject] = useState(project);
+  const [isEditing, setIsEditing] = useState(false);
+  const [thisProject, setThisProject] = useState(project);
 
-    return (
-        <>
-            {isEditing ? (
-                <ProjectEditForm project={thisProject} setIsEditing={setIsEditing} setThisProject={setThisProject} />
-            ) : (
-                <ProjectCard project={thisProject} isEditable={isEditable} setIsEditing={setIsEditing} />
-            )}
-        </>
-    )
+  return (
+    <>
+      {isEditing ? (
+        <ProjectEditForm
+          project={thisProject}
+          setIsEditing={setIsEditing}
+          setThisProject={setThisProject}
+        />
+      ) : (
+        thisProject && (
+          <ProjectCard
+            project={thisProject}
+            isEditable={isEditable}
+            setIsEditing={setIsEditing}
+            setThisProject={setThisProject}
+          />
+        )
+      )}
+    </>
+  );
 }
 export default Project;
--- front/src/components/projects/ProjectCard.js
+++ front/src/components/projects/ProjectCard.js
@@ -1,34 +1,46 @@
 import { Card, Row, Button, Col } from "react-bootstrap";
+// import * as Api from "../../api"
+function ProjectCard({ project, setIsEditing, isEditable, setThisProject }) {
+  const { title, description, from_date, to_date } = project;
 
-function ProjectCard ({ project, setIsEditing, isEditable }) {
-  const { title, description } = project;
-  const fromDate = project.from_date;
-  const toDate = project.to_date;
-
+  const deleteHandler = async () => {
+    // await Api.delete(`project/${project.id}`)
+    if (window.confirm("Are you sure you want to delete this project?")) {
+      setThisProject(null);
+    }
+  };
   return (
     <>
       <Card.Text>
-          <Row className="align-items-center">
-            <Col>
-              <span>{title}</span>
-              <br />
-              <span className="text-muted">{description}</span>
-              <br />
-              <span className="text-muted">{fromDate + " ~ " + toDate}</span>
-            </Col>
-            {isEditable && (
+        <Row className="align-items-center">
+          <Col>
+            <span>{title}</span>
+            <br />
+            <span className="text-muted">{description}</span>
+            <br />
+            <span className="text-muted">{`${from_date} ~ ${to_date}`}</span>
+          </Col>
+          {isEditable && (
             <Col lg={1} xs={true}>
-                <Button
-                  variant="outline-info"
-                  size="sm"
-                  onClick={() => setIsEditing(true)}
-                  className="mr-3"
-                >
-                  편집
-                </Button>
+              <Button
+                variant="outline-info"
+                size="sm"
+                onClick={() => setIsEditing(true)}
+                className="mr-3"
+              >
+                편집
+              </Button>
+              <Button
+                variant="outline-danger"
+                size="sm"
+                className="mr-3 mt-1"
+                onClick={() => deleteHandler()}
+              >
+                삭제
+              </Button>
             </Col>
-            )}
-          </Row>
+          )}
+        </Row>
       </Card.Text>
     </>
   );
