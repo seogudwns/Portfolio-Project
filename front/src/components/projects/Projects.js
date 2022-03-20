6162fa010fe65a962aa64fd6ee5fc301c0dfebb3
Hayeong Kim <fibonacci2246@gmail.com>
2022-03-20 18:19:29 +0900

    fix: remove dummydata

--- front/src/components/projects/Projects.js
+++ front/src/components/projects/Projects.js
@@ -5,33 +5,6 @@ import ProjectAddForm from "./ProjectAddForm";
 // import * as Api from "../../api";
 
 function Projects({ portfolioOwnerId, isEditable }) {
-  //! 더미데이터
-  const dummyProjects = [
-    {
-      id: 1, // project의 고유 id
-      user_id: "abc1", // portfolioOwnerId
-      title: "JS 프로젝트",
-      description: "프론트엔드 역량을 위해 자바스크립트 프로젝트 진행",
-      from_date: "2021-05-20",
-      to_date: "2021-06-20",
-    },
-    {
-      id: 2,
-      user_id: "abc1",
-      title: "express 프로젝트",
-      description: "백엔드 역량을 위해 express 프로젝트 진행",
-      from_date: "2021-07-20",
-      to_date: "2021-08-20",
-    },
-    {
-      id: 3,
-      user_id: "abc1",
-      title: "react 프로젝트",
-      description: "프론트엔드 역량을 위해 리액트 프로젝트 진행",
-      from_date: "2021-09-20",
-      to_date: "2021-10-20",
-    },
-  ];
 
   const [isAdding, setIsAdding] = useState(false);
   const [projects, setProjects] = useState(dummyProjects);
