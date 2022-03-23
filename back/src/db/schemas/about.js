6e5cec7923745c03aab7575cc4791c69bf28b30a
haechan <kiki9510@gmail.com>
2022-03-23 16:56:26 +0900

    about schema 코드 작성

---
+++ back/src/db/schemas/about.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const AboutSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    blog: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    skill: {
+        type: [String],
+        required: false,
+        default: [],
+    },
+    position: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    hobby: {
+        type: [String],
+        required: false,
+        default: [],
+    },
+});
+
+const AboutModel = new model("About", AboutSchema);
+
+export { AboutModel };
