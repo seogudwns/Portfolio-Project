f7954752f9fd75395422e87092d53c97c98f01f1
leewol <lsee5265@gmail.com>
2022-03-25 07:53:30 +0900

    User MVP - Network 무한 스크롤 (기능 추가)

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -5,32 +5,35 @@ import { Container, Row } from "react-bootstrap";
 import * as Api from "../../api";
 import UserCard from "./UserCard";
 import { UserStateContext } from "../../App";
+import NetworkScroll from "./NetworkScroll";
 
 function Network() {
-  const navigate = useNavigate();
-  const userState = useContext(UserStateContext);
-  // useState 훅을 통해 users 상태를 생성함.
-  const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
+    const userState = useContext(UserStateContext);
+    // useState 훅을 통해 users 상태를 생성함.
+    const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
-    if (!userState.user) {
-      navigate("/login");
-      return;
-    }
-    // "userlist" 엔드포인트로 GET 요청을 하고, users를 response의 data로 세팅함.
-    Api.get("userlist").then((res) => setUsers(res.data));
-  }, [userState, navigate]);
+    useEffect(() => {
+        // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
+        if (!userState.user) {
+            navigate("/login");
+            return;
+        }
+        // "userlist" 엔드포인트로 GET 요청을 하고, users를 response의 data로 세팅함.
+        Api.get("userlist").then((res) => {
+            setUsers(res.data);
+        });
+    }, [userState, navigate]);
 
-  return (
-    <Container fluid>
-      <Row xs="auto" className="jusify-content-center">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} isNetwork />
-        ))}
-      </Row>
-    </Container>
-  );
+    return (
+        <>
+            {users.length !== 0 ? (
+                <NetworkScroll users={users} />
+            ) : (
+                <span>Loading...</span>
+            )}
+        </>
+    );
 }
 
 export default Network;
