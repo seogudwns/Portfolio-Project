731ab78d2167dd5733994a4ca9a43a76b7609b32
aldrn <aldrn29@gmail.com>
2022-03-25 22:25:15 +0900

    Certificate 경로 수정

--- front/src/components/certificates/Certificates.js
+++ front/src/components/certificates/Certificates.js
@@ -8,41 +8,50 @@ function Certificates({ portfolioOwnerId, isEditable }) {
   const [certificates, setCertificates] = useState([]);
   const [isAdding, setIsAdding] = useState(false);
 
+  const handleAddClick = () => {
+    setIsAdding((current) => !current);
+  };
+
   useEffect(() => {
-    // "certificateList/유저id"로 GET 요청
-    Api.get("certificateList", portfolioOwnerId).then(res => {
+    // "certificates/list/유저id"로 GET 요청
+    Api.get("certificates/list", portfolioOwnerId).then(res => {
       setCertificates(res.data);
     });
   }, [portfolioOwnerId]);
 
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>자격증</Card.Title>
-        {certificates.map(certificate => (
-          <Certificate
-            certificate={certificate}
-            setCertificates={setCertificates}
-            isEditable={isEditable}
-          />
-        ))}
-        {isEditable && (
-          <Row className="mt-3 text-center mb-4">
-            <Col sm={{ span: 20 }}>
-              <Button variant="primary" onClick={() => setIsAdding(true)}>
-                +
-              </Button>
-            </Col>
-          </Row>
-        )}
-        {isAdding && (
-          <CertificateAddForm
-            portfolioOwnerId={portfolioOwnerId}
-            setCertificates={setCertificates}
-            setIsAdding={setIsAdding}
-          />
-        )}
-      </Card.Body>
+      <Card className="mb-2">
+        <Card.Body>
+          <Card.Title className="component-name">자격증</Card.Title>
+          {certificates.map(certificate => (
+            <Certificate
+              certificate={certificate}
+              setCertificates={setCertificates}
+              isEditable={isEditable}
+            />
+          ))}
+          {isEditable && (
+              <Row className="mt-3 text-center mb-4">
+                  <Col sm={{ span: 20 }}>
+                      <Button
+                          className="btn btn-primary"
+                          onClick={handleAddClick}
+                      >
+                          {!isAdding ? "+" : "-"}
+                      </Button>
+                  </Col>
+              </Row>
+          )}
+          {isEditable && isAdding ? (
+              <CertificateAddForm
+                  portfolioOwnerId={portfolioOwnerId}
+                  setIsAdding={setIsAdding}
+                  setCertificates={setCertificates}
+              />
+          ) : (
+              <></>
+          )}
+        </Card.Body>
     </Card>
   );
 }
