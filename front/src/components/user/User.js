9997a9fad7dde833eac99730dfe04458cab1283d
leewol <lsee5265@gmail.com>
2022-03-27 04:34:37 +0900

    유저 탈퇴 기능 추가

--- front/src/components/user/User.js
+++ front/src/components/user/User.js
@@ -4,33 +4,34 @@ import UserCard from "./UserCard";
 import * as Api from "../../api";
 
 function User({ portfolioOwnerId, isEditable }) {
-  // useState 훅을 통해 isEditing 상태를 생성함.
-  const [isEditing, setIsEditing] = useState(false);
-  // useState 훅을 통해 user 상태를 생성함.
-  const [user, setUser] = useState(null);
+    // useState 훅을 통해 isEditing 상태를 생성함.
+    const [isEditing, setIsEditing] = useState(false);
+    // useState 훅을 통해 user 상태를 생성함.
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    // "users/유저id" 엔드포인트로 GET 요청을 하고, user를 response의 data로 세팅함.
-    Api.get("users", portfolioOwnerId).then((res) => setUser(res.data));
-  }, [portfolioOwnerId]);
+    useEffect(() => {
+        // "users/유저id" 엔드포인트로 GET 요청을 하고, user를 response의 data로 세팅함.
+        Api.get("users", portfolioOwnerId).then((res) => setUser(res.data));
+    }, [portfolioOwnerId]);
 
-  return (
-    <>
-      {isEditing ? (
-        <UserEditForm
-          user={user}
-          setIsEditing={setIsEditing}
-          setUser={setUser}
-        />
-      ) : (
-        <UserCard
-          user={user}
-          setIsEditing={setIsEditing}
-          isEditable={isEditable}
-        />
-      )}
-    </>
-  );
+    return (
+        <>
+            {isEditing ? (
+                <UserEditForm
+                    user={user}
+                    setIsEditing={setIsEditing}
+                    setUser={setUser}
+                />
+            ) : (
+                <UserCard
+                    user={user}
+                    setIsEditing={setIsEditing}
+                    isEditable={isEditable}
+                    portfolioOwnerId={portfolioOwnerId}
+                />
+            )}
+        </>
+    );
 }
 
 export default User;
