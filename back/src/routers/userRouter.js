8feb584932c4abe66d58a731b3c71a0abfa27286
HyoungJun Seo <seogudwns12@gmail.com>
2022-03-26 22:07:26 +0900

    일단 만들었는데 잘 작동하는지 체크할 시간이 없다...ㅠㅠ

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,6 +1,7 @@
 import is from "@sindresorhus/is";
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
+import { addTokenBlackList } from "../middlewares/TokenBlackList";
 import { userAuthService } from "../services/userService";
 
 const userAuthRouter = Router();
@@ -49,6 +50,23 @@ userAuthRouter.post("/login", async (req, res, next) => {
         next(error);
     }
 });
+
+userAuthRouter.post("/logout", addTokenBlackList, async (req, res, next) => {
+    try {
+        const userToken = req.headers["authorization"]?.split(" ")[1] ?? "null";
+        const token = await userAuthService.logoutUser({ userToken });
+
+        if (!token) {
+            const errorMessage = "...이게 일어나면 정말 이상한거임.";
+            return { errorMessage }; //! 어떤 상황에서 애러가 날까?
+        }
+
+        res.status(200).json("로그아웃 성공!");
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 userAuthRouter.get("/list", login_required, async (req, res, next) => {
     
@@ -155,16 +173,17 @@ userAuthRouter.get(
     }
 );
 
-userAuthRouter.delete("/:id", login_required, async (req, res, next) => {
+userAuthRouter.delete("/:id", login_required, addTokenBlackList, async (req, res, next) => {
     try {
         const user_id = req.params.id;
+        const userToken = req.headers["authorization"]?.split(" ")[1] ?? "null";
 
         if (req.currentUserId !== user_id) {
             throw new Error("접근권한이 없습니다.");
         }
 
-        const deletdUser = await userAuthService.deleteUser({ user_id });
-
+        const deletdUser = await userAuthService.deleteUser({ user_id, userToken });
+        //! 토큰을 막는 것은 유저정보가 db에서 삭제되는 것과 함께 일어나야 하는 일이기 때문에 service 아래에서 진행.
         if (deletdUser.errorMessage) {
             throw new Error(deletdUser.errorMessage);
         }
